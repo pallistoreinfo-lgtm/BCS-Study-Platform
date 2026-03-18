659d1238a0bcaaa67c50b737768713d01c22332b
pallistoreinfo-lgtm <pallistoreinfo@gmail.com>
2026-03-18 17:01:07 +0600

    Update and rename next.config.ts to next.config.js

--- next.config.ts
+++
@@ -1,6 +0,0 @@
-import type { NextConfig } from 'next'
-const nextConfig: NextConfig = {
-  images: { remotePatterns: [{ protocol: 'https', hostname: '*.supabase.co' }] },
-  experimental: { serverActions: { allowedOrigins: ['localhost:3000'] } },
-}
-export default nextConfig
